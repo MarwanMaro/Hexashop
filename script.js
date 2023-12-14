7db6c27099ad5d1d27da67df2d8f07ef598101c7
MarwanMaro <90068982+MarwanMaro@users.noreply.github.com>
2023-12-14 11:57:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$('.owl-carousel').owlCarousel({
+    nav: true,
+    margin:70,
+    loop:true,
+    // navText: ["<div class='nav-button owl-prev'>‹</div>", "<div class=' owl-next'>›</div>"],
+    // autoWidth: true,
+    dots: false,
+    items:3
+})
+
+
+
